b60f4ca08d2bf1f1d0dbdfa05325af46a09f06ba
RadtkeN <raddnc@gmail.com>
2022-05-26 19:26:41 -0500

    sorting returned data and sending to the dom in readable format. set state abb, lat, and lon to pull image from folder as well to use the google maps rest api. added simple error alert to make sure everything is working.

--- TestZipCode/script.js
+++ TestZipCode/script.js
@@ -12,7 +12,21 @@ async function getData() {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
+        let stateAbbrev = data["places"][0]["state abbreviation"];
+        let long = data["places"][0]["longitude"];
+        let lati = data["places"][0]["latitude"];
+        const imgLoc = "states/" + stateAbbrev + ".svg";
+        document.getElementById('city').textContent = data.places[0]["place name"];
+        document.getElementById('state').textContent = data.places[0].state;
+        document.getElementById('stateAbb').textContent = stateAbbrev;
+        document.getElementById('country').textContent = data.country;
+        document.getElementById('countryAbb').textContent = data["country abbreviation"];
+        document.getElementById('postal').textContent = data['post code'];
+        document.getElementById('longitude').textContent = long;
+        document.getElementById('latitude').textContent = lati;
+        document.getElementById('img').src = imgLoc;
     } catch (error) {
-
+        alert("There is an error with the API call, please contact me. " + error);
+        console.log(error);
     }
 }
