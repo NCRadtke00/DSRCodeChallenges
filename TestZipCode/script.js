9cdb18dbd99e0bda0f55cd32c56936c4fcf04f2e
RadtkeN <raddnc@gmail.com>
2022-05-26 19:23:56 -0500

    added a try catch for error handling, set up fetch and added onClick to submit button. pulls json from entered zipcode.

--- TestZipCode/script.js
+++ TestZipCode/script.js
@@ -2,10 +2,17 @@ console.log("running");
 async function getData() {
     let zipCode = document.getElementById('zip').value;
     if (zipCode == "") {
-        alert("5 digit Zip Code must be filled in.")
+        alert("5 digit USA Zip Code must be filled in.")
         return false;
     }
     let link = 'https://api.zippopotam.us/US/';
     let url = link + zipCode;
     console.log(url);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+
+    }
 }
